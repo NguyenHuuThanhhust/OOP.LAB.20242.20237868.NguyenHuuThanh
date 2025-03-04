219eb4db596923a2234509d9642bf1e6f9cd1f4e
NguyenHuuThanhhust <codexdrako@gmail.com>
2025-03-04 21:54:35 +0700

    Update Bai225Calculator.java

--- Lab01/Bai225Calculator.java
+++ Lab01/Bai225Calculator.java
@@ -1,31 +1,46 @@
 import javax.swing.JOptionPane;
 
 public class Bai225Calculator {
-
     public static void main(String[] args) {
-        String strNum1, strNum2;
-        String strNotification = "Ban da nhap 2 so: ";
-
-        strNum1 = JOptionPane.showInputDialog(null, "Xin hay nhap so thu 1 :", "Nhap so thu 1", JOptionPane.QUESTION_MESSAGE);
-        double num1 = Double.parseDouble(strNum1);          strNotification += strNum1 + " and ";
-
-        strNum2 = JOptionPane.showInputDialog(null, "Xin hay nhap so thu 2 :", "Nhap so thu 2", JOptionPane.QUESTION_MESSAGE);
-        double num2 = Double.parseDouble(strNum2); 
-        strNotification += strNum2;
-
-        double sum = num1 + num2;
-        double difference = num1 - num2;
-        double product = num1 * num2;
-        double quotient = num2 != 0 ? num1 / num2 : Double.NaN; 
-
-        String result = strNotification + "\n" +
-                       "Tong: " + sum + "\n" +
-                       "Hieu: " + difference + "\n" +
-                       "Tich: " + product + "\n" +
-                       "Thuong: " + (quotient == Double.NaN ? "Khong the chia cho 0" : quotient);
-
-        JOptionPane.showMessageDialog(null, result, "Calculation Results", JOptionPane.INFORMATION_MESSAGE);
-
+        String strNum1 = JOptionPane.showInputDialog("Enter 1st number:");
+        double num1 = Double.parseDouble(strNum1);
+        
+        String strNum2 = JOptionPane.showInputDialog("Enter 2nd number:");
+        double num2 = Double.parseDouble(strNum2);
+        
+        String operator = JOptionPane.showInputDialog("Choose an operator (+, -, *, /):");
+        
+        double result = 0;
+        boolean validOperation = true;
+        
+        switch (operator) {
+            case "+":
+                result = num1 + num2;
+                break;
+            case "-":
+                result = num1 - num2;
+                break;
+            case "*":
+                result = num1 * num2;
+                break;
+            case "/":
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    JOptionPane.showMessageDialog(null, "Cannot divide by zero.", "Error", JOptionPane.ERROR_MESSAGE);
+                    validOperation = false;
+                }
+                break;
+            default:
+                JOptionPane.showMessageDialog(null, "Invalid operator!", "Error", JOptionPane.ERROR_MESSAGE);
+                validOperation = false;
+        }
+        
+        if (validOperation) {
+            String equation = num1 + " " + operator + " " + num2 + " = " + result;
+            JOptionPane.showMessageDialog(null, "Result: " + result + "\n" + equation, "Calculation Result", JOptionPane.INFORMATION_MESSAGE);
+        }
+        
         System.exit(0);
     }
 }
